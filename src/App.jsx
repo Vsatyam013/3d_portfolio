25d0d04037fd5cc06ea9d590eb934ba707d6a9bc
vsatyam013 <vsatyam013@gmail.com>
2025-01-10 12:19:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import {BrowserRouter} from 'react-router-dom';
+import {About, Contact, Experience, Feedbacks, 
+      Hero, Navbar, Tech, Works, StarsCanvas} from './components'
+
+const App =() => {
+
+  return (
+    <div>
+      <BrowserRouter>
+        <div className='relative z-0 bg-primary'>
+          <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+            <Navbar/>
+            <Hero/>
+          </div>
+          <About></About>
+          <Experience></Experience>
+          <Tech></Tech>
+          <Works></Works>
+          <Feedbacks></Feedbacks>
+          <div className='relative z-0'>
+            <Contact></Contact>
+            <StarsCanvas></StarsCanvas>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
